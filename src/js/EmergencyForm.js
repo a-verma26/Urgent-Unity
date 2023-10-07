25688f20fd42390112bbf55cac3a5eb0b783b56e
a-verma26 <iamabhishekverma98@gmail.com>
2023-10-07 17:22:30 -0400

    images added.........................

--- src/js/EmergencyForm.js
+++ src/js/EmergencyForm.js
@@ -3,7 +3,6 @@ import HospitalNotification from './HospitalNotification';
 import MapComponent from './MapComponent';
 import RealTimeChat from './RealTimeChat';
 import '../scss/EmergencyForm.scss';
-import SpeechToTextDemo from './SpeechToText';
 import SpeechToText from './SpeechToText';
 
 class EmergencyForm extends Component {
@@ -38,7 +37,7 @@ class EmergencyForm extends Component {
           };
           // Reverse geocoding using Google Maps Geocoding API
           const reverseGeocodingUrl = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${userLocation.latitude},${userLocation.longitude}&key=${apiKey}`;
-          
+
           try {
             const response = await fetch(reverseGeocodingUrl);
             const data = await response.json();
@@ -87,86 +86,84 @@ class EmergencyForm extends Component {
 
     return (
       <div className='headContainer'>
-      {!isSubmitClicked && <div className='emergencyFormContainer'>
+        {isSubmitClicked && <div className='emergencyFormContainer'>
           <form onSubmit={this.handleSubmit}>
-          <label>
-            Type of Emergency:
-            <select name="emergencyType" value={emergencyType} onChange={this.handleChange}>
-              <option value="">Select an Emergency Type</option>
-              <option value="Cardiac Arrest">Cardiac Arrest</option>
-              <option value="Choking">Choking</option>
-              <option value="Trauma">Trauma</option>
-              <option value="Heart Attack">Heart Attack</option>
-              <option value="Accident">Accident</option>
-              <option value="Seizure">Seizure</option>
-              <option value="Stroke">Stroke</option>
-              <option value="Diabetic Emergency">Diabetic Emergency</option>
-              <option value="Allergic Reaction">Allergic Reaction (Anaphylaxis)</option>
-            </select>
-          </label>
-          {/* <SpeechToTextDemo/> */}
-          <label>
-            Additional Details:
-            <textarea
-              name="emergencyDetails"
-              value={emergencyDetails}
-              onChange={this.handleChange}
-              rows="4"
-              cols="50"
-              placeholder="Provide additional details or notes about the emergency..."
-            />
-          </label>
-        
-          <button type="submit">Submit</button>
-        </form>
-        <SpeechToText/>
+            <label>
+              Type of Emergency:
+              <select name="emergencyType" value={emergencyType} onChange={this.handleChange}>
+                <option value="">Select an Emergency Type</option>
+                <option value="Cardiac Arrest">Cardiac Arrest</option>
+                <option value="Choking">Choking</option>
+                <option value="Trauma">Trauma</option>
+                <option value="Heart Attack">Heart Attack</option>
+                <option value="Accident">Accident</option>
+                <option value="Seizure">Seizure</option>
+                <option value="Stroke">Stroke</option>
+                <option value="Diabetic Emergency">Diabetic Emergency</option>
+                <option value="Allergic Reaction">Allergic Reaction (Anaphylaxis)</option>
+              </select>
+            </label>
+            <label>
+              Additional Details:
+              <textarea
+                name="emergencyDetails"
+                value={emergencyDetails}
+                onChange={this.handleChange}
+                rows="4"
+                cols="50"
+                placeholder="Provide additional details or notes about the emergency..."
+              />
+            </label>
+
+            <button type="submit">Submit</button>
+          </form>
+          <SpeechToText />
         </div>}
-        {isSubmitClicked && (
+        {!isSubmitClicked && (
+
           <div className='mapContainer'>
-            <MapComponent emergencyType={emergencyType} />
             {userLocation && userLocation.address && (
               <div>
-                <h3>Your Current Location</h3>
-                <p>Address: {userLocation.address}</p>
+                <b><span>Your Current Location:   </span></b>
+                <span>{userLocation.address}</span>
               </div>
             )}
-            
-            <div>
+            <span style={{ marginRight: '30px' }}>
               Would you like to notify the nearby hospital to prepare for the patient's arrival in advance?
-              You can also choose to notify the hospital after the CPR practitioner arrives.
+            </span>
+            <div className='buttonContainer'>
+              <button onClick={this.handleNotifyHospital}>Notify Nearby Hospital</button>
             </div>
-            
-            <button onClick={this.handleNotifyHospital}>Notify Nearby Hospital</button>
-            <div>Your current location has been shared with nearby CPR practitioners.</div>
-            <div>Your current location has been shared with nearby Ambulance. Estimated time to arrive is 14 minutes </div>
-            
-           
-            
+            <div style={{ marginBottom: '10px' }}>Your current location has been shared with nearby CPR practitioners.</div>
+
+            <MapComponent emergencyType={emergencyType} />
+            <div>Estimated time to Ambulance arrive is 14 minutes </div>
+
           </div>
         )}
-      
-    
-      {notifyNearbyHospital && <HospitalNotification />}
-      {isSubmitClicked && <RealTimeChat emergencyType={emergencyType} />}
-     {!isSubmitClicked &&  <div className='footer'>
-  <h3>Frequently Asked Questions</h3>
-  <ul>
-    <li>
-      <strong>Q: How does the app work?</strong>
-      <p>A: The app connects users with nearby CPR practitioners and emergency services to provide timely assistance during emergencies.</p>
-    </li>
-    <li>
-      <strong>Q: What information is shared with CPR practitioners?</strong>
-      <p>A: Only essential information, such as your location and emergency type, is shared with CPR practitioners to ensure a prompt response.</p>
-    </li>
-    <li>
-      <strong>Q: How is my data handled?</strong>
-      <p>A: Your data is handled securely and is only used for emergency response purposes. We prioritize user privacy and data protection.</p>
-    </li>
-    {/* Add more FAQs as needed */}
-  </ul>
-</div>}
-    </div>
+
+
+        {notifyNearbyHospital && <HospitalNotification />}
+        {isSubmitClicked && <RealTimeChat emergencyType={emergencyType} />}
+        {!isSubmitClicked && <div className='footer'>
+          <h3>Frequently Asked Questions</h3>
+          <ul>
+            <li>
+              <strong>Q: How does the app work?</strong>
+              <p>A: The app connects users with nearby CPR practitioners and emergency services to provide timely assistance during emergencies.</p>
+            </li>
+            <li>
+              <strong>Q: What information is shared with CPR practitioners?</strong>
+              <p>A: Only essential information, such as your location and emergency type, is shared with CPR practitioners to ensure a prompt response.</p>
+            </li>
+            <li>
+              <strong>Q: How is my data handled?</strong>
+              <p>A: Your data is handled securely and is only used for emergency response purposes. We prioritize user privacy and data protection.</p>
+            </li>
+            {/* Add more FAQs as needed */}
+          </ul>
+        </div>}
+      </div>
 
     );
   }
